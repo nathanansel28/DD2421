af7ee3133895683769edcea67dccb220f6447477
shaniatirkey <1007069@mymail.sutd.edu.sg>
2025-02-20 21:24:37 +0100

    added comments

--- lab3/tools.py
+++ lab3/tools.py
@@ -33,7 +33,8 @@ class BoostClassifier(object):
     def trainClassifier(self, X, labels):
         rtn = BoostClassifier(self.base_classifier, self.T)
         rtn.nbr_classes = np.size(np.unique(labels))
-        rtn.classifiers, rtn.alphas = trainBoost(self.base_classifier, X, labels, self.T)
+        rtn.classifiers, rtn.alphas = trainBoost(
+            self.base_classifier, X, labels, self.T)
         rtn.trained = True
         return rtn
 
@@ -50,35 +51,40 @@ ASSIGNMENT 1
 #     labels - N vector of class labels
 # out:    mu - C x d matrix of class means (mu[i] - class i mean)
 #      sigma - C x d x d matrix of class covariances (sigma[i] - class i sigma)
+
+
 def mlParams(
     X: np.ndarray, labels: np.ndarray, W: Optional[np.ndarray] = None
 ) -> Tuple[np.ndarray, np.ndarray]:
     """ Calculates and returns the ML-estimates of the mu and sigma of a dataset. """
 
-    assert(X.shape[0]==labels.shape[0])
+    assert (X.shape[0] == labels.shape[0])
     classes, inverse = np.unique(labels, return_inverse=True)
-    C = len(classes)
-    N, d = np.shape(X)
+    C = len(classes)    # no. of unique classes
+    N, d = np.shape(X)  # no. of data points & features
 
     if W is None:
-        W = np.ones((N, 1)) / float(N)
+        W = np.ones((N, 1)) / float(N)  # assume equal weights
 
     mu = np.zeros((C, d))
     sigma = np.zeros((C, d, d))
 
-    counts = np.zeros(C)
-    np.add.at(mu, inverse, X)
+    counts = np.zeros(C)            # creates a zero array of no. of classes
+    np.add.at(mu, inverse, X)       # add data point to respective class mean
+    # count no.of samples belonging to each class
     np.add.at(counts, inverse, 1)
-    mu /= counts[:, None]
+    mu /= counts[:, None]           # divide by class count to get average
 
     for idx, k in enumerate(classes):
-        mask = labels == k
-        W_k = W[mask]
-        X_k = X[mask]
-        
+        mask = labels == k          # select data points belonging to class k
+        W_k = W[mask]               # extract feature vectors for class k
+        X_k = X[mask]               # extract weight vector for class k
+
+        # sum of weights for class k
         W_k_sum = np.sum(W_k)
-        mu[idx] = np.sum(W_k * X_k, axis=0) / W_k_sum
-        
+        mu[idx] = np.sum(W_k * X_k, axis=0) / \
+            W_k_sum       # compute weighted mean
+
         diff = X_k - mu[idx]
         sigma[idx] = np.diag(np.sum(W_k * (diff ** 2), axis=0) / W_k_sum)
 
@@ -92,23 +98,28 @@ ASSIGNMENT 2
 """
 # in: labels - N vector of class labels
 # out: prior - C x 1 vector of class priors
+
+
 def computePrior(
     labels: np.ndarray, W: Optional[np.ndarray] = None
 ) -> np.ndarray:
     """ Estimates and returns the class prior in X (ignore the W argument). """
-    Npts = labels.shape[0]
+    Npts = labels.shape[0]                              # get no. of data points
     if W is None:
-        W = np.ones((Npts, 1))/Npts
+        W = np.ones((Npts, 1))/Npts                     # assign equal weights
     else:
-        assert(W.shape[0] == Npts)
-    classes = np.unique(labels)
-    Nclasses = np.size(classes)
+        # check if W has same no.of elements as labels
+        assert (W.shape[0] == Npts)
+    classes = np.unique(labels)                         # distinct class labels
+    Nclasses = np.size(classes)                         # no. unique classes
 
+    # zero arr to store the priors
     prior = np.zeros((Nclasses, 1))
+    # compute weighted priors for each class
     for idx, k in enumerate(classes):
         mask = labels == k
         prior[idx] = np.sum(W[mask]) / np.sum(W)
-    
+
     return prior
 
 
@@ -127,21 +138,22 @@ def classifyBayes(
 
     arrays = [
         (
-            - 1/2 * np.log(np.linalg.det(sigma[k])) 
-            - 1/2 * np.einsum('ij,jk,ik->i', (X - mu[k]), np.linalg.inv(sigma[k]), (X - mu[k]))
+            - 1/2 * np.log(np.linalg.det(sigma[k]))
+            - 1/2 * np.einsum('ij,jk,ik->i',
+                              (X - mu[k]), np.linalg.inv(sigma[k]), (X - mu[k]))
             + np.log(prior[k])
         ) for k in range(prior.shape[0])
     ]
-    
+
     log_prob = np.vstack(arrays)
-    h = np.argmax(log_prob, axis=0) 
+    # find index of max value for each sample
+    h = np.argmax(log_prob, axis=0)
     return h
 
-    
 
 """
 ============
-ASSIGNMENT 4
+ASSIGNMENT 5
 ============
 """
 # in: base_classifier - a classifier of the type that we will boost, e.g. BayesClassifier
@@ -150,17 +162,19 @@ ASSIGNMENT 4
 #                   T - number of boosting iterations
 # out:    classifiers - (maximum) length T Python list of trained classifiers
 #              alphas - (maximum) length T Python list of vote weights
+
+
 def trainBoost(
-    base_classifier: Union[BayesClassifier, BoostClassifier], 
-    X: np.ndarray, 
-    labels: np.ndarray, 
+    base_classifier: Union[BayesClassifier, BoostClassifier],
+    X: np.ndarray,
+    labels: np.ndarray,
     T: int = 10
 ) -> Tuple[Union[BayesClassifier, BoostClassifier], List[np.ndarray]]:
     # these will come in handy later on
     Npts, Ndims = np.shape(X)
 
-    classifiers = [] # append new classifiers to this list
-    alphas = [] # append the vote weight of the classifiers to this list
+    classifiers = []  # append new classifiers to this list
+    alphas = []  # append the vote weight of the classifiers to this list
 
     # The weights for the first iteration
     wCur = np.ones((Npts, 1)) / float(Npts)
@@ -170,8 +184,10 @@ def trainBoost(
         classifiers.append(base_classifier.trainClassifier(X, labels, wCur))
 
         # do classification for each point
+        # the latest classifier predicts labels for all pts in X
         vote = classifiers[-1].classify(X)
 
+        # find misclassified pts
         incorrect = (vote != labels).astype(float)
         error = np.sum(wCur.flatten() * incorrect) / np.sum(wCur)
 
@@ -181,12 +197,12 @@ def trainBoost(
         elif error == 1:
             error = 1 - 1e-10
 
-        alpha = 0.5 * (np.log((1 - error) / error))
+        alpha = 0.5 * (np.log((1 - error) / error))     # compute importance
         alphas.append(alpha)
 
         # Update sample weights
         wCur = wCur * np.exp(alpha * incorrect[:, np.newaxis])
-        
+
         # Normalize weights
         wCur /= np.sum(wCur)
 
@@ -199,9 +215,9 @@ def trainBoost(
 #    Nclasses - the number of different classes
 # out:  yPred - N vector of class predictions for test points
 def classifyBoost(
-    X: np.ndarray, 
-    classifiers: List[Union[BayesClassifier, BoostClassifier]], 
-    alphas: List[float], 
+    X: np.ndarray,
+    classifiers: List[Union[BayesClassifier, BoostClassifier]],
+    alphas: List[float],
     Nclasses: int
 ):
     Npts = X.shape[0]
@@ -211,13 +227,12 @@ def classifyBoost(
     if Ncomps == 1:
         return classifiers[0].classify(X)
     else:
-        votes = np.zeros((Npts,Nclasses))
+        votes = np.zeros((Npts, Nclasses))
         for t in range(Ncomps):
-            predictions = classifiers[t].classify(X)  # Get predictions from classifier t
+            # Get predictions from classifier t
+            predictions = classifiers[t].classify(X)
             for i in range(Npts):
                 votes[i, predictions[i]] += alphas[t]  # Add weighted vote
         yPred = np.argmax(votes, axis=1)
 
         return yPred
-
-
